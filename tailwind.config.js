d9aaa11af93e0fca1a14a15daa253ba40e5f2796
suren <surenpal666@gmail.com>
2026-01-20 22:03:27 +0900

    tailwind 4 is upgraded

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+};
